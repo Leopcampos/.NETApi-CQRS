262ffa789a48540862a31b36dfa779cc55303e27
Leonardo Pereira Campos <leonardocampos@fiotec.fiocruz.br>
2024-03-05 12:05:45 -0300

    feat: Criando dbcontext, IUnitOfWork, primeira migration e o primeiro controller.

---
+++ src/CleanArch.CrossCutting/AppDependencies/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Abstractions;
+using CleanArch.Infrastructure.Context;
+using CleanArch.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.CrossCutting.AppDependencies;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var sqlConnection = configuration.GetConnectionString("ClearArcht");
+
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnection));
+
+        services.AddScoped<IMemberRepository, MemberRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
--- src/CleanArch.Domain/Abstractions/IMemberRepository.cs
+++ src/CleanArch.Domain/Abstractions/IMemberRepository.cs
@@ -2,11 +2,11 @@
 
 namespace CleanArch.Domain.Abstractions;
 
-public interface IMemberRepository
+public interface IMemberRepository : IDisposable
 {
-    Task<IEnumerable<Member>> GetAll();
+    Task<IEnumerable<Member>> GetMembers();
     Task<Member> GetMemberById(int memberId);
     Task<Member> AddMember(Member member);
-    Task<Member> UpdateMember(Member member);
-    Task DeleteMember(int idmemberId);
+    void UpdateMember(Member member);
+    Task<Member> DeleteMember(int memberId);
 }
